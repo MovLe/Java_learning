a46b7071bfc186080732ef43e6b5650a1ae1dec6
Movle <richard.xiu.520@gmail.com>
2020-01-08 01:45:24 +0800

    Java值传递之引用数据类型传递

---
+++ src/main/java/com/movle/java/basic/ObjectParms.java
@@ -0,0 +1,52 @@
+package com.movle.java.basic;
+
+/**
+ * @ClassName ObjectParms
+ * @MethodDesc: 参数传递之值传递
+ * @Author macbook
+ * @Date 1/8/20 1:37 上午
+ * @Version 1.0
+ * @Email ma342221@163.com
+ **/
+
+
+public class ObjectParms {
+
+
+
+    public static void mulAge(Animals a){
+        int age=a.getAge();
+
+        a.setAge(++age);
+
+        System.out.println("mulAge中的年龄为"+a.getAge());
+    }
+
+
+    public static void main(String[] args) {
+
+        Animals a= new Animals();
+        a.setAge(10);
+
+
+        mulAge(a);
+
+        System.out.println("main函数中的年龄为"+a.getAge());
+
+    }
+
+
+}
+
+class Animals{
+
+    private int age;
+
+    public void setAge(int age1){
+        this.age = age1;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+}
