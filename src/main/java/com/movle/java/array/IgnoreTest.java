1cc984d8d17dc7bb81ab1471b2189aa7fd073e13
Movle <richard.xiu.520@gmail.com>
2020-01-10 00:55:42 +0800

    ignore test

---
+++ src/main/java/com/movle/java/array/IgnoreTest.java
@@ -0,0 +1,18 @@
+package com.movle.java.array;
+
+/**
+ * @ClassName IgnoreTest
+ * @MethodDesc: TODO IgnoreTest功能介绍
+ * @Author Movle
+ * @Date 1/10/20 12:54 上午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+
+
+public class IgnoreTest {
+
+    public static void main(String[] args) {
+        System.out.println("ignore Text");
+    }
+}
