65bf3fc9b727c8a65ab75a6542beb6e8bb755556
Movle <richard.xiu.520@gmail.com>
2020-11-07 20:40:24 +0800

    泛型通配符+增加注释

--- src/main/java/com/movle/javareview/genericAPI/Test02.java
+++ src/main/java/com/movle/javareview/genericAPI/Test02.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 /**
  * @ClassName Test02
  * @MethodDesc: 泛型通配符
+ *      泛型的通配符
+ * 	        ？：代表任意的数据类型
+ *      使用方式：
+ * 	        不能创建对象使用
+ * 	        只能作为方法的参数使用
+ *      泛型的上限限定
+ * 	        ？ extends E 代表使用的泛型只能是E类型的子类/本身
+ *      泛型类的下限限定
+ * 	        ？ super E 代表使用的泛型只能是E类型的父类/本身
  * @Author Movle
  * @Date 11/7/20 7:13 下午
  * @Version 1.0
